5b7d1fa369db7095bc9b49c26bc7ea9f3aa41ddb
Bella Gillard <qiman2001@gmail.com>
2023-08-03 18:00:49 +0100

    Part 2 done!

--- actions.py
+++ actions.py
@@ -1,8 +1,28 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from engine import Engine
+    from entity import Entity
+
+
+
 class Action:
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        """Performs this action with the objects needed to determine its scope.
+
+        Args:
+            engine (Engine): scope that this action is being performed in.
+            entity (Entity): object preforming the action.
+            
+            Must Be overridden by Action subclasses.
+        """
+        raise NotImplementedError()
 
 class EscapeAction(Action):
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise SystemExit()
 
 
 class MovementAction(Action):
@@ -11,3 +31,14 @@ class MovementAction(Action):
         
         self.dx = dx
         self.dy = dy
+        
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+        
+        if not engine.game_map.in_bounds(dest_x,dest_y):
+            return
+        if not engine.game_map.tiles["walkable"][dest_x,dest_y]:
+            return 
+
+        entity.move(self.dx, self.dy)
--- input_handlers.py
+++ input_handlers.py
@@ -9,12 +9,12 @@ from actions import Action, EscapeAction, MovementAction
 class EventHandler(tcod.event.EventDispatch[Action]):
     def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
         raise SystemExit()
-    
+
     def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
         action: Optional[Action] = None
-        
+
         key = event.sym
-        
+
         if key == tcod.event.K_UP:
             action = MovementAction(dx=0, dy=-1)
         elif key == tcod.event.K_DOWN:
@@ -23,8 +23,8 @@ class EventHandler(tcod.event.EventDispatch[Action]):
             action = MovementAction(dx=-1, dy=0)
         elif key == tcod.event.K_RIGHT:
             action = MovementAction(dx=1, dy=0)
-        
+
         elif key == tcod.event.K_ESCAPE:
             action = EscapeAction()
-        
+
         return action

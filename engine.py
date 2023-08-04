b61d4fafc3e93bf16bdc916ce54a4f491617b210
Bella Gillard <qiman2001@gmail.com>
2023-08-04 13:01:02 +0100

    Part 4 done

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@ from typing import Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
 
 from entity import Entity
@@ -15,6 +16,7 @@ class Engine:
         self.event_handler = event_handler
         self.game_map = game_map
         self.player = player
+        self.update_fov()
 
     def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
@@ -24,12 +26,28 @@ class Engine:
                 continue
             
             action.perform(self, self.player)
+            
+            self.update_fov()
+            
+    def update_fov(self) -> None:
+        """Recompute the visible area based on the players point of view."""
+        
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8,    
+        )
+        
+        # If a tile is "visible", it should be added to "explored".
+        self.game_map.explored |= self.game_map.visible
 
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
         
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+            if self.game_map.visible[entity.x,entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
+        
         context.present(console)
 
         console.clear()
--- tile_types.py
+++ tile_types.py
@@ -14,6 +14,7 @@ tile_dt = np.dtype(
         ("walkable", bool),
         ("transparent", bool),
         ("dark", graphic_dt),
+        ("light", graphic_dt),
     ]
 )
 
@@ -22,15 +23,26 @@ def new_tile(
     *,
     walkable: int,
     transparent: int,
-    dark: tuple[int, tuple[int, int, int], tuple[int, int, int]]
+    dark: tuple[int, tuple[int, int, int], tuple[int, int, int]],
+    light: tuple[int, tuple[int, int, int], tuple[int, int, int]]
 ) -> np.ndarray:
     """Helper function for defining individual types """
-    return np.array((walkable, transparent, dark), dtype=tile_dt)
+    return np.array((walkable, transparent, dark, light), dtype=tile_dt)
 
 
-floor = new_tile(walkable=True, transparent=True, dark=(
+# SHROUD represents unexplored, unseen tiles
+SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 
-    ord(" "), (255, 255, 255), (50, 50, 150)))
+floor = new_tile(
+    walkable=True,
+    transparent=True,
+    dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    light=(ord(" "), (255, 255, 255), (200, 180, 50))
+)
 
-wall = new_tile(walkable=False, transparent=False,
-                dark=(ord(" "), (255, 255, 255), (0, 0, 100)))
+wall = new_tile(
+    walkable=False,
+    transparent=False,
+    dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    light=(ord(" "), (255, 255, 255), (130, 110, 50))
+)
